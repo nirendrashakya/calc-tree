5ca3647f3dd07c62c853e2990ecbedec7ddf4b62
nirendra shakya <nirendra.shakya@park.edu>
2019-10-11 00:59:32 -0500

    Added njsproj

--- index.ts
+++ index.ts
@@ -1,36 +1,37 @@
 import { Node } from './node';
 export class Runner {
-  static Run() {
-    console.log('\n================ Start ================');
-    const root = new Node<number>(undefined, 'root');
-    const a = new Node<number>(undefined, 'a');
-    a.recalc = 2;
-    const b = new Node<number>(undefined, 'b');
-    const c = new Node<number>(undefined, 'c');
-    const d = new Node<number>(undefined, 'd');
-    const e = new Node<number>(10, 'e');
-    const f = new Node<number>(undefined, 'f');
-    const g = new Node<number>(1, 'g');
-    const h = new Node<number>(undefined, 'h');
+    static Run() {
+        console.log('\n================ Start ================');
+        const root = new Node<number>(undefined, 'root');
+        const a = new Node<number>(undefined, 'a');
+        a.recalc = 2;
+        const b = new Node<number>(undefined, 'b');
+        const c = new Node<number>(undefined, 'c');
+        const d = new Node<number>(undefined, 'd');
+        const e = new Node<number>(10, 'e');
+        const f = new Node<number>(undefined, 'f');
+        const g = new Node<number>(1, 'g');
+        const h = new Node<number>(undefined, 'h');
 
-    root.add(a).add(b);
+        root.add(a).add(b);
 
-    a.add(c).add(f);
-    
-    b.add(c);
+        a.add(c).add(f);
 
-    c.add(d);
+        b.add(c);
 
-    d.add(a).add(e);
+        c.add(d);
 
-    f.add(g).add(h);
-    
-    h.add(e);
+        d.add(a).add(e);
 
-    root.run()
-    console.log('Final value =>', root.data);
-    console.log('================ End =================\n');
-  }
+        f.add(g).add(h);
+
+        h.add(e);
+
+        root.run()
+        console.log('Final value =>', root.data);
+        console.log('================ End =================\n');
+        process.stdin.read();
+    }
 }
 
 Runner.Run();
